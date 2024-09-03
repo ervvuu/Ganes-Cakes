b69f5242f3a4eebc1d67c876cb5430c938281a2d
Erva <152181523+ervvuu@users.noreply.github.com>
2024-09-03 23:22:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+  const searchIcon = document.querySelector('.search-icon');
+const searchForm = document.querySelector('.search-form');
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector(".navbar");
+
+searchIcon.addEventListener("click", () => {
+searchForm.classList.add("active");
+navbar.classList.remove("active");
+})
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.add("active");
+    searchForm.classList.remove("active");
+    
+})  
+window.onscroll = ()=>{
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
+}
+
+/*  document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const closeIcon = document.getElementById('close-icon');
+    const navbar = document.querySelector('.navbar');
+
+    menuIcon.addEventListener('click', function() {
+        navbar.classList.add('active');
+        menuIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
+    });
+
+    closeIcon.addEventListener('click', function() {
+        navbar.classList.remove('active');
+        closeIcon.style.display = 'none';
+        menuIcon.style.display = 'block';
+    });
+});  */
